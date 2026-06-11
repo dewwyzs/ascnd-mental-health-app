6de12c291744433c7a658b4d75fcb9b52cc46214
dewyzs <ziyuyang201@gmail.com>
2026-06-11 10:35:39 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-export const metadata = {
-  title: 'Ascend',
-  description: 'Girl athlete mental health platform',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   )
+}
+
+export const metadata = {
+  title: 'Ascend',
+  description: 'Girl athlete mental health platform',
 }
